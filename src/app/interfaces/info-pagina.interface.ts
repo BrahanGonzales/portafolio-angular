6d8fe694dc9af2adb7b5db68380db6565e9c2eae
brahangonzales <brahangonzales@gmail.com>
2023-02-17 09:59:30 -0500

    Fin sección 4

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,12 @@
+export interface infoPagina{
+    titulo?: string;
+    email?: string;
+    nombre_corto?: string;
+    pagina_autor?: string;
+    facebook?: string;
+    twitter?: string;
+    turbl?: string;
+    instagram?: string;
+    equipo_trabajo?: any[];
+    
+}
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { InfoPaginaService } from 'src/app/services/info-pagina.service';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  
   anio: number = new Date().getFullYear();
+
+
+  constructor( public _servicio: InfoPaginaService ){
+    
+  }
 }
